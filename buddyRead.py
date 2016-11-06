83ebb8edc14299836655acbc0373b76915cad1d4
Derek <zengzhuoxi@gmail.com>
2016-11-05 23:25:16 -0400

    Changed readcomments to readcontent.

--- buddyRead.py
+++ buddyRead.py
@@ -13,16 +13,15 @@ def launch():
     questionText = "What subreddit would you like to browse?"
     return question(questionText)
 
-
 @ask.intent("subRedditIntent")
-def subRedditBrowse():
-    statementText = "You have entered the Donald"
+def subRedditBrowse(subreddit):
+    statementText = "You have entered {}".format(subreddit)
     return statement(statementText)
 
-@ask.intent("readComments")
-def readComments():
-    commentText1 = "comment 1"
-    commentText2 = "comment 2"
+@ask.intent("readContentIntent")
+def readContent():
+    content1 = "comment 1"
+    content2 = "comment 2"
     return statement(commentText1)
 
 @ask.session_ended
@@ -30,7 +29,6 @@ def readComments():
 def session_ended():
     statementText = "Goodbye, buddy"
     return statement(statementText)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
